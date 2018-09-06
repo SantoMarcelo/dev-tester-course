9792842092e265d3d61c0d2a7a02923871f545c1
Marcelo <santos.marcelooliveira@outlook.com>
2018-09-05 22:03:29 -0300

    aula2

--- aula-1/tests/features/step_definitions/contato_steps.rb
+++ aula-1/tests/features/step_definitions/contato_steps.rb
@@ -1,17 +1,26 @@
-Dado("que {string} é meu novo contato") do |nome|                            
+Dado("que {string} é meu novo contato") do |nome| 
   @nome = nome
-  find_element(text: '').set(nome)
 end                                                                            
                                                                                  
 Dado("este contato possui os seguintes dados:") do |table|                     
-  # table is a Cucumber::MultilineArgument::DataTable                          
-  pending # Write code here that turns the phrase above into concrete actions  
-end                                                                            
-                                                                                 
+ @contato = table.rows_hash
+ 
+end
+
 Quando("faço o cadastro deste novo contato") do                                
-  pending # Write code here that turns the phrase above into concrete actions  
+  visit 'http://127.0.0.1:8080/form-novo.html'  
+  find('input[placeholder="Nome Completo"]').set(@nome)
+  find('input[placeholder="Email"]').set(@contato[:email])
+  find('input[placeholder="Celular"]').set(@contato[:celular])
+  #find('select[]')
+  
+  
+  tipo_contato = find('#tipo')
+  tipo_contato.find('option', text: @contato[:tipo]).select_option
+  click_on 'Cadastrar'
+ 
 end                                                                            
                                                                                  
 Então("devo ver a seguinte mensagem de sucesso {string}") do |mensagem|          
-  pending # Write code here that turns the phrase above into concrete actions  
+ expect(page).to have_content mensagem
 end                                                                            
